55286a051f133a1429b60fea400d90a94787969e
mallick-portfolio <tamal1714019@gmail.com>
2025-03-22 10:43:37 +0600

    added traffic generator script

---
+++ traffic_genrator/trafic_gen.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+echo "Usage: $0 <target> <interval_in_minutes>"
+exit 1
+fi
+
+TARGET=$1
+INTERVAL=$2
+
+while true; do
+    TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+    RESPONSE= curl -s "$TARGET"
+    echo "[$TIMESTAMP] $RESPONSE"
+    sleep "$INTERVAL"
+done
